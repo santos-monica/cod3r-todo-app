0817cf1ec0d65fdbc2dd6c7c8be001495f3bac4e
Mônica <monica.santos@itlab.com.br>
2019-03-02 15:58:17 -0300

    uso de rotas, componente de listagem e form

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
   <div>
-    <h1>Sobre</h1>  
+    <PageHeader name='About' small='Us' />
+
+    <h3>Nossa história</h3>
+    <p>Blah</p>
+    <h3>Missão e Visão</h3>
+    <p>Blah</p>
   </div>
 )
