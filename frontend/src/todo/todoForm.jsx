ed0e18c892ca1d04987c9bcae434dbe6ad560b20
Mônica <monica.santos@itlab.com.br>
2019-03-04 18:41:47 -0300

    uso do middleware para promise

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,41 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription } from './todoActions'
+import { changeDescription, search } from './todoActions'
 
-const TodoForm = props => {
-  const keyHandler = (e) => {
+class TodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.keyHandler = this.keyHandler.bind(this)
+  }
+  
+  keyHandler(e) {
     e.key === 'Enter' 
-      ? (e.shiftKey ? props.handleSearch() : props.handleAdd()) 
-        : (e.key === 'Escape' ? props.handleClear() : null)
+      ? (e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()) 
+        : (e.key === 'Escape' ? this.props.handleClear() : null)
   }
 
-  return (
-    <div role='form' className='todoForm'>
-      <Grid cols='12 9 10'>
-        <input
-          type='text'
-          id='description'
-          className='form-control'
-          placeholder='Add a todo'
-          value={props.description}
-          onKeyUp={keyHandler}
-          onChange={props.changeDescription}
-          />
-      </Grid>
-      <Grid cols='12 3 2'>
-        <IconButton style='primary' onClick={props.handleAdd} icon='plus' />
-        <IconButton style='info' onClick={props.handleSearch} icon='search' />
-        <IconButton style='default' onClick={props.handleClear} icon='close' />
-      </Grid>
-    </div>
-  )
+  componentWillMount() {
+    this.props.search()
+  }
+
+  render() {
+    return (
+      <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+          <input
+            type='text'
+            id='description'
+            className='form-control'
+            placeholder='Add a todo'
+            value={this.props.description}
+            onKeyUp={this.keyHandler}
+            onChange={this.props.changeDescription}
+            />
+        </Grid>
+        <Grid cols='12 3 2'>
+          <IconButton style='primary' onClick={this.props.handleAdd} icon='plus' />
+          <IconButton style='info' onClick={this.props.handleSearch} icon='search' />
+          <IconButton style='default' onClick={this.props.handleClear} icon='close' />
+        </Grid>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({ description: state.todo.description })
 const mapDispatchToProps = dispatch => 
-  bindActionCreators({ changeDescription }, dispatch)
+  bindActionCreators({ changeDescription, search }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,24 +1,11 @@
-const INITIAL_STATE = {
-  description: 'Ler livro',
-  list:[{
-      _id: 1,
-      description: 'Pagar fatura do cartão',
-      done: true,
-    }, {
-      _id: 2,
-      description: 'Reunião as 13h',
-      done: false,
-    }, {
-      _id: 3,
-      description: 'Médico as 16h',
-      done: false,
-    }]
-}
+const INITIAL_STATE = { description: ' ', list:[] }
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'DESCRIPTION_CHANGE':
       return { ...state, description: action.payload }
+    case 'TODO_SEARCHED':
+      return { ...state, list: action.payload.data }
     default:
       return state
   }
